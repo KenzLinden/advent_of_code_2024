38ef65fa53278e081d5e162da32a1590ac358909
KenzLinden <mlinden1403@gmail.com>
2024-12-05 10:19:35 +1100

    Solution 3 finished

--- solutions/3.py
+++ solutions/3.py
@@ -14,26 +14,20 @@ def q1(input_text):
     
     return total
 
-test_input = "do()xmul(2,4)&mul[3,7]!^mul(2,1)don't()don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-
 # part 2
 def q2(input_text):
     
     input_text = "do()" + input_text + "don't()" # just nicer to wrap
-    input_text = input_text.strip()
-    activated_pattern = r"do\(\)(.*?)don't\(\)" # only gets items between do()...don't()
-    matches = re.findall(activated_pattern, input_text)
-    
-
-
     
     total = 0
     nums_pattern = r'mul\((\d+),(\d+)\)'
-    for match in matches:
+    section_pattern = r"do\(\)(.*?)don't\(\)"
+    
+    # find all sections that are activated (re.DOTALL does multiline check)
+    # and then find mults within that
+    for match in re.findall(section_pattern, input_text, re.DOTALL):
         for nums in re.findall(nums_pattern, match):
-            total += (int(nums[0]) * int(nums[1]))
-
-
+            total += int(nums[0]) * int(nums[1])
     return total 
 
 print("Solution to q1: ", q1(input_text))
